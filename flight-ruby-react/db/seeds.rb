f73e3e8ef4cbd3935e01b93a8eee3eef38c195c2
YuriiHorbach <yuriihorbach.career@gmail.com>
2025-03-06 15:13:06 +0100

    Initial commit

---
+++ flight-ruby-react/db/seeds.rb
@@ -0,0 +1,42 @@
+Airline.create([
+  {
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  },
+  {
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+  },
+  {
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png"
+  },
+  {
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png"
+  },
+  {
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png"
+  },
+  {
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png"
+  }
+])
+
+reviews = Review.create([
+  {
+    title: 'Great airline',
+    description: 'good',
+    score: 5,
+    airline: Airline.first
+  },
+
+  {
+    title: 'Bad airline',
+    description: 'bad',
+    score: 1,
+    airline: Airline.first
+  }
+])
